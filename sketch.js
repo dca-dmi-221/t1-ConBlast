0f68cdc79b2c480d5dcdc5a07dfc66ff9e9901c9
Christian Castro <85885192+ConBlast@users.noreply.github.com>
2022-03-31 23:03:33 -0500

    Agregando listas y botones funcionales

--- sketch.js
+++ sketch.js
@@ -3,11 +3,11 @@ let canciones = [];
 let index = 0;
 
 function preload(){
-  canciones[0] = loadSound("Song1.mp3");
-  canciones[1] = loadSound("Song2.mp3");
-  canciones[2] = loadSound("Song3.mp3");
-  canciones[3] = loadSound("Song4.mp3");
-  canciones[4] = loadSound("Song5.mp3");
+  canciones[0] = loadSound("Trap1.mp3");
+  canciones[1] = loadSound("Trap2.mp3");
+  canciones[2] = loadSound("Trap3.mp3");
+  canciones[3] = loadSound("Trap4.mp3");
+  canciones[4] = loadSound("Trap5.mp3");
 }
 
 
@@ -15,7 +15,11 @@ function setup() {
   createCanvas(1600, 800);
   img = loadImage('/Logo.png')
   botonPlay=createButton("Play");
-  botonPlay.mousePressed(Play)
+  botonPlay.mousePressed(Play);
+  botonStop=createButton("Stop");
+  botonStop.mousePressed(Stop);
+  botonNext=createButton("Next");
+  botonNext.mousePressed(Next);
 }
 
 function draw() {
@@ -24,5 +28,21 @@ function draw() {
 }
 
 function Play(){
-  canciones[2].play();
+  canciones[index].play();
 }
+
+function Stop(){
+  canciones[index].stop();
+}
+
+function Next(){
+  Stop();
+  if(indice<canciones.length-1){
+  indice+=1;
+  }
+  else {
+    indice=0;
+  }
+  Play();
+}
+
