39f8641e0a1db62fa253967da5ee4d3134f89356
Christian Castro <85885192+ConBlast@users.noreply.github.com>
2022-04-02 15:52:40 -0500

    Cambios y pruebas

--- example.js
+++ example.js
@@ -1,14 +1,6 @@
 let img;
 let canciones = [];
 let index = 0;
-// let peaky;
-
-let boton;
-
-class Pantallas{
-  
-}
-
 
 function preload(){
   canciones[0] = loadSound("Trap1.mp3");
@@ -16,30 +8,42 @@ function preload(){
   canciones[2] = loadSound("Trap3.mp3");
   canciones[3] = loadSound("Trap4.mp3");
   canciones[4] = loadSound("Trap5.mp3");
-  
-}
 
+}
 
 function setup() {
-  createCanvas(1600, 800);
+  createCanvas(1800, 800);
   img = loadImage('/Logo.png')
-  
-  botonPlay=createButton("Play");
+
+  botonPlay=createButton("");
   botonPlay.mousePressed(Play);
+  botonPlay.position(715, 538);
+  botonPlay.size(70, 70);
+  applyButtonStyle(botonPlay, "Start2.png");
+
+
   botonStop=createButton("Stop");
   botonStop.mousePressed(Stop);
+  
+
   botonNext=createButton("Next");
   botonNext.mousePressed(Next);
-  
-  //peaky = new Boton(849,538,"Start.png","Pausa.png");
-  
-  boton = new Boton(600,600,"/Start.png","/Pause.png")
+
 }
+
+function applyButtonStyle(button, image) {
+  button.style("background-color", "#FFFFFF");
+  button.style("border-radius", "50%");
+  button.style("background-position", "center");
+  button.style("background-image", "url(" + image + ")");
+  button.style("background-repeat", "no-repeat");
+}
+
 
 function draw() {
   background(0,0,0);
+  
   image(img,330,120);
-  boton.draw2()
 }
 
 function Play(){
@@ -60,26 +64,3 @@ function Next(){
   }
   Play();
 }
-
-class Boton {
-  constructor(x,y,botonplay,botonpause) {
-  this.x = x;
-  this.y = y;
-  this.botonplay = loadImage(botonplay);
-  this.botonpause = loadImage(botonpause);
-  
-}
-
-getX(){
-  return this.x;
-}
-getY(){
-  return this.y;
-}
-
-draw2() {
-  
-  image(this.botonPlay,200,100)
-}
-}
-
