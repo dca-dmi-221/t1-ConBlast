f26f484b80c61c53d170f0418f9a35035036404b
Christian Castro <85885192+ConBlast@users.noreply.github.com>
2022-04-02 18:04:22 -0500

    Listo ahora a organizar mas

--- Code1.js
+++ Code1.js
@@ -3,28 +3,30 @@ let canciones = [];
 let index = 0;
 
 function preload(){
-  canciones[0] = loadSound("Trap1.mp3");
-  canciones[1] = loadSound("Trap2.mp3");
-  canciones[2] = loadSound("Trap3.mp3");
-  canciones[3] = loadSound("Trap4.mp3");
-  canciones[4] = loadSound("Trap5.mp3");
+  canciones[0] = loadSound("PeakyBlinders/Electro1.mp3");
+  canciones[1] = loadSound("PeakyBlinders/Electro2.mp3");
+  canciones[2] = loadSound("PeakyBlinders/Electro3.mp3");
+  canciones[3] = loadSound("PeakyBlinders/Electro4.mp3");
+  canciones[4] = loadSound("PeakyBlinders/Electro5.mp3");
 
 }
 
 function setup() {
   createCanvas(1800, 800);
-  img = loadImage('/Logo.png')
+  img = loadImage('Elementos/Logo.png')
 
   botonPlay=createButton("");
   botonPlay.mousePressed(Play);
-  botonPlay.position(715, 538);
-  botonPlay.size(70, 70);
-  applyButtonStyle(botonPlay, "Start2.png");
+  botonPlay.position(900, 538);
+  botonPlay.size(80, 80);
+  applyButtonStyle(botonPlay, "Elementos/Start.png");
 
 
   botonStop=createButton("Stop");
   botonStop.mousePressed(Stop);
-  
+  botonStop.position(820, 538);
+  botonStop.size(80, 80);
+  applyButtonStyle(botonStop, "Elementos/Pause.png");
 
   botonNext=createButton("Next");
   botonNext.mousePressed(Next);
@@ -42,8 +44,7 @@ function applyButtonStyle(button, image) {
 
 function draw() {
   background(0,0,0);
-  
-  image(img,330,120);
+  image(img,475,101);
 }
 
 function Play(){
