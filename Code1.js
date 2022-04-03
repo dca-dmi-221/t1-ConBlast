59bd8521fd76bdc2d149f22fa7a4cdb1700a2849
Christian Castro <85885192+ConBlast@users.noreply.github.com>
2022-04-03 01:59:47 -0500

    Mas botones 👍

--- Code1.js
+++ Code1.js
@@ -1,6 +1,11 @@
 let img;
+let backgrounds = [];
 let canciones = [];
 let index = 0;
+
+
+
+//Cargar archivos
 
 function preload(){
   canciones[0] = loadSound("PeakyBlinders/Electro1.mp3");
@@ -9,7 +14,13 @@ function preload(){
   canciones[3] = loadSound("PeakyBlinders/Electro4.mp3");
   canciones[4] = loadSound("PeakyBlinders/Electro5.mp3");
 
+  backgrounds[0] = loadImage("Elementos/BG2.png");
+  backgrounds[1] = loadImage("Elementos/BG3.png");
+  backgrounds[2] = loadImage("Elementos/BG4.png");
+  
 }
+
+// Setup
 
 function setup() {
   createCanvas(1800, 800);
@@ -21,17 +32,36 @@ function setup() {
   botonPlay.size(80, 80);
   applyButtonStyle(botonPlay, "Elementos/Start.png");
 
-
-  botonStop=createButton("Stop");
+  botonStop=createButton("");
   botonStop.mousePressed(Stop);
   botonStop.position(820, 538);
   botonStop.size(80, 80);
   applyButtonStyle(botonStop, "Elementos/Pause.png");
 
-  botonNext=createButton("Next");
+  botonNext=createButton("");
   botonNext.mousePressed(Next);
+  botonNext.position(980, 538);
+  botonNext.size(80, 80);
+  applyButtonStyle(botonNext, "Elementos/Next.png");
 
+  botonLast=createButton("");
+  botonLast.mousePressed(Last);
+  botonLast.position(740, 538);
+  botonLast.size(80, 80);
+  applyButtonStyle(botonLast, "Elementos/Last.png");
 }
+
+
+// Draw
+
+function draw() {
+  background(0,0,0);
+
+  image(img,475,101);
+  
+}
+
+// Funciones
 
 function applyButtonStyle(button, image) {
   button.style("background-color", "#FFFFFF");
@@ -41,14 +71,9 @@ function applyButtonStyle(button, image) {
   button.style("background-repeat", "no-repeat");
 }
 
-
-function draw() {
-  background(0,0,0);
-  image(img,475,101);
-}
-
 function Play(){
   canciones[index].play();
+  
 }
 
 function Stop(){
@@ -62,6 +87,17 @@ function Next(){
   }
   else {
     index=0;
+  }
+  Play();
+}
+
+function Last(){
+  Stop();
+  if(index<canciones.length-1){
+  index-=1;
+  }
+  else {
+    index=4;
   }
   Play();
 }
